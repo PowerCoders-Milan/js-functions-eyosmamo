95dd274fa1d24ee37292ac50d68dcce279dbe760
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-16 14:32:27 +0200

    ex celsius_farenheit done

--- 03-celsius_fahrenheit/celsius_fahrenheit.js
+++ 03-celsius_fahrenheit/celsius_fahrenheit.js
@@ -1,12 +1,28 @@
 // define a function multiplyByNineFifths(number)
+const multiplyByNineFifths = x => {
 
-    //return number*(9/5)
+    return (x * (9 / 5));
+}
+//return number*(9/5)
 
 // define a function getFahrenheit(celsius)
+const Fahrenheit = y => {
+    return (multiplyByNineFifths(y) + 32);
+}
 
-    // return multiplyByNineFifths(celsius) + 32;
+// return multiplyByNineFifths(celsius) + 32;
 
-console.log() // call getFahrenheit();
+console.log(`${Fahrenheit(10)}`)
+
+// call getFahrenheit();
 
 
 // do the same thing starting from Fahrenheit to Celsius
+const getFahrenheit = z => {
+
+    return (z - 32);
+}
+const Celsius = z => {
+    return (getFahrenheit(z) * (5 / 9));
+}
+console.log(`${Celsius(10)}`)
