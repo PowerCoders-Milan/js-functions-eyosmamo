68c2eb79bcf629f06f39dbfcf9e66f70a03e2625
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-16 10:46:01 +0200

    ex.can i vote done

--- 01-can_you_vote/can_you_vote.js
+++ 01-can_you_vote/can_you_vote.js
@@ -1,15 +1,16 @@
 // define a function that will check if I can vote or not
-const canIVote = age => {
-    // define a variable legalAge
 
-    // create if else statement that console.log() if you can vote or not
-    if (condition) {
-        
+const canIVote = age => {
+    const legalage = 18;
+    if (age >= legalage) {
+        console.log("You can vote");
     } else {
-        
+        console.log("You can not vote");
     }
 }
 
-// define a variable for your age
 
+// define a variable for your age
+canIVote(35)
 // call the function and see if you can vote
+console.log(`age`);
